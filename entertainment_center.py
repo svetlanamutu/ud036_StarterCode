6e3880a3d2e697db97cfac267c3c7d3c9d36b8b6
Svetlana Galuzinschii <galuzinschiisvetlana@gmail.com>
2018-10-02 16:21:09 -0500

    Changed code formatting and URL of the trailer of Life Of Pi movie
    Changed code formatting and URL of the trailer of Life Of Pi movie.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,24 +1,38 @@
 import media
 import fresh_tomatoes
 
-# 'media.py' and 'fresh_tomatoes.py' files must be in the same directory with the 'entertainment_center.py' file.
-# Also, the images for two movies (lifeofpi.jpg and TheIntouchables.jpg) must be in the same folder with 'entertainment_center.py' and 'media.py' and 'fresh_tomatoes.py' files (I could not find good links for them). 
+# 'media.py' and 'fresh_tomatoes.py' files must be in the same directory with
+# the'entertainment_center.py' file.
+# Also, the images for two movies (lifeofpi.jpg and TheIntouchables.jpg)
+# must be in the same folder with 'entertainment_center.py' and
+# 'media.py' and 'fresh_tomatoes.py' files (I could not find good links
+# for them).
 
 # Defining four Movie objects; Movie class is defined in 'media' module
-life_of_pi=media.Movie("Life Of Pi", "A story of a boy and a tiger in the ocean", "lifeofpi.jpg", "https://www.youtube.com/watch?v=8eEXxqF6g9Y")
+life_of_pi = media.Movie("Life Of Pi",
+                         "A story of a boy and a tiger in the ocean",
+                         "LifeOfPi.jpg",
+                         "https://www.youtube.com/watch?v=3mMN693-F3U")
 
-the_intouchables=media.Movie("The Intouchables", "A man-nurse of a sick man helps the patient siritually", "TheIntouchables.jpg","https://www.youtube.com/watch?v=34WIbmXkewU")
+the_intouchables = media.Movie("The Intouchables",
+                               "A man-nurse of a sick man helps the patient siritually",  # noqa
+                               "TheIntouchables.jpg",
+                               "https://www.youtube.com/watch?v=34WIbmXkewU")
 
-avatar=media.Movie("Avatar", "A marine on an alien planet", "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg","http://www.youtube.com/watch?v=-9ceBgWV8io")
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",  # noqa
+                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
 
-nu_pogodi=media.Movie("Nu Pogodi", "A favorite childrens' cartoon in Russia", "https://upload.wikimedia.org/wikipedia/commons/2/2b/Nu%2C_pogodi%21_logo.png", "https://www.youtube.com/watch?v=fZTBffmVWeE")
+nu_pogodi = media.Movie("Nu Pogodi",
+                        "A favorite childrens' cartoon in Russia",
+                        "https://upload.wikimedia.org/wikipedia/commons/2/2b/Nu%2C_pogodi%21_logo.png",  # noqa
+                        "https://www.youtube.com/watch?v=fZTBffmVWeE")
 
 # Creating a list of Movie objects from the four individual Movie objects
-movies=[life_of_pi,the_intouchables,avatar,nu_pogodi]
+movies = [life_of_pi, the_intouchables, avatar, nu_pogodi]
 
-# Calling the function open_movies_page defined in 'fresh_tomatoes.py' file. This function creates the html file with all the movies and opens it in a browser.
+# Calling the function open_movies_page defined in 'fresh_tomatoes.py' file.
+# This function creates the html file with all the movies and
+# opens it in a browser.
 fresh_tomatoes.open_movies_page(movies)
-
-
-
-
--- media.py
+++ media.py
@@ -1,9 +1,33 @@
 import webbrowser
+
+
 class Movie():
-    def __init__(svetlana, movie_title, movie_storyline, poster_image, trailer_youtube):
-        svetlana.title=movie_title
-        svetlana.storyline =movie_storyline
-        svetlana.poster_image_url=poster_image
-        svetlana.trailer_youtube_url=trailer_youtube
+    """
+      Inputs:
+              None.
+      Behavior:
+              Constructor method for Movie class. It initializes an
+              instance of Movie object with its title, storyline,
+              poster image, and trailer
+      Output:
+              No output.
+     """
+    def __init__(svetlana, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        svetlana.title = movie_title
+        svetlana.storyline = movie_storyline
+        svetlana.poster_image_url = poster_image
+        svetlana.trailer_youtube_url = trailer_youtube
+
     def show_trailer(self):
+        """
+          Inputs:
+                  None.
+          Behavior:
+                  Opens a browser and plays the trailer by reading
+                  the trailer_utube_url property of the instance
+                  of the Movie class.
+          Output:
+                  No output.
+        """
         webbrowser.open(self.trailer_youtube_url)
